1612a4451a0ac8256ea7962be1b31c49f2190433
Huy TA QUOC <htquoc@linagora.com>
2020-07-23 16:10:36 +0700

    https://github.com/OpenPaaS-Suite/esn-frontend-inbox/issues/34: Added usage of the `esn-app-grid` web component

--- src/frontend/js/modules/application-menu.js
+++ src/frontend/js/modules/application-menu.js
@@ -1,131 +1,21 @@
-(function(angular) {
+const { applyPolyfills, defineCustomElements } = require('esn-frontend-application-grid/loader');
 
-  'use strict';
+applyPolyfills().then(() => defineCustomElements());
 
-  angular.module('esn.application-menu', [
-    'op.dynamicDirective',
-    'feature-flags'
-  ])
-
-  .constant('POPOVER_APPLICATION_MENU_OPTIONS', {
-    animation: 'am-fade',
-    placement: 'bottom',
-    template: require("../../views/modules/application-menu/application-menu.pug"),
-    html: false,
-    trigger: 'manual',
-    autoClose: true,
-    prefixEvent: 'application-menu',
-    container: 'body'
-  })
-  .constant('APP_MENU_OPEN_EVENT', 'application-menu.open')
-
-  .factory('applicationMenuTemplateBuilder', function(featureFlags, _) {
-    var template =
-    '<div>' +
-    '<a href="<%- href %>" target="<%- target %>" rel="<%- rel %>">' +
-    '<img class="esn-application-menu-icon" src="<%- iconURL %>" fallback-src="/images/application.png"/>' +
-    '<span class="label" translate>' +
-    '<%- label %>' +
-    '</span>' +
-    '</a>' +
-    '</div>';
-
-    var svgTemplate =
-      '<div>' +
-      '<a href="<%- href %>" target="<%- target %>" rel="<%- rel %>">' +
-      '<div class="esn-application-menu-icon" ng-include="\'<%- iconURL %>\'"></div>' +
-      '<span class="label" translate>' +
-      '<%- label %>' +
-      '</span>' +
-      '</a>' +
-      '</div>';
-
-      return function(href, icon, label, flag, isDisplayedByDefault) {
-        var iconURL, aHref, aTarget;
-        var aRel = '';
-      var iconUrlTemplate = '/images/application-menu/<%- icon %>-icon.svg';
-      var defaultValue = angular.isDefined(isDisplayedByDefault) ? isDisplayedByDefault : true;
-      var isActive = defaultValue;
-
-      if (angular.isDefined(flag)) {
-        isActive = featureFlags.isOn(flag) === undefined ? defaultValue : featureFlags.isOn(flag);
-      }
-
-      if (!isActive) {
-        return '';
-      }
-
-      if (icon.url) {
-        iconURL = icon.url;
-      } else if (icon.name) {
-        iconURL = _.template(iconUrlTemplate)({icon: icon.name});
-      }
-
-      if (angular.isObject(href)) {
-        aHref = href.url;
-        aTarget = href.target || '';
-        aRel = href.rel || aRel;
-      } else {
-        aHref = href;
-        aTarget = '';
-      }
-
-      return iconURL && iconURL.match(/.\S+\.svg/) ?
-      _.template(svgTemplate)({ href: aHref, target: aTarget, rel: aRel, iconURL: iconURL, label: label }) :
-      _.template(template)({ href: aHref, target: aTarget, rel: aRel, iconURL: iconURL, label: label });
-    };
-  })
-
-  .directive('applicationMenuToggler', function($rootScope, $document, $popover,
-    POPOVER_APPLICATION_MENU_OPTIONS, APP_MENU_OPEN_EVENT) {
+// TODO: Write tests for this
+angular.module('esn.application-menu', [])
+  .directive('applicationMenuToggler', function($log) {
     return {
       restrict: 'E',
       scope: true,
       replace: true,
       template: require("../../views/modules/application-menu/application-menu-toggler.pug"),
-      link: function(scope, element) {
-        var backdrop = angular.element('<div id="application-menu-backdrop" class="modal-backdrop in visible-xs">'),
-        body = $document.find('body').eq(0),
-            popover = $popover(element, POPOVER_APPLICATION_MENU_OPTIONS);
+      link: function($scope) {
+        $scope.appGridItems = process.env.APP_GRID_ITEMS;
 
-            popover.$scope.$on('application-menu.show.before', function() {
-              body.append(backdrop);
-              $rootScope.$broadcast(APP_MENU_OPEN_EVENT);
-              scope.$applyAsync();
-            });
-
-            popover.$scope.$on('application-menu.hide.before', function() {
-              backdrop.remove();
-              scope.$applyAsync();
-            });
-
-            element.click(popover.toggle.bind(null, null));
-      }
-    };
-  })
-
-  .directive('forceCloseOnLinksClick', function($timeout) {
-    return {
-      restrict: 'A',
-      link: function(scope, element) {
-        $timeout(function() {
-          element.find('a').click(scope.$parent.$hide.bind(null, null));
-        }, 0, false);
-      }
-    };
-  })
-
-  .directive('forceMarginLeft', function($timeout) {
-    return {
-      restrict: 'A',
-      link: function(scope, element, attrs) {
-        $timeout(function() {
-          var offset = element.offset();
-
-          element.offset({top: offset.top, left: offset.left - attrs.forceMarginLeft});
-        }, 0, false);
+        if (!$scope.appGridItems) {
+          $log.error('The environment variable APP_GRID_ITEMS has not been defined yet, and the application grid will break.')
+        }
       }
     };
   });
-
-})(angular);
