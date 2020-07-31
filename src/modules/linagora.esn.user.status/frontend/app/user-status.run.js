1e28d4b72a6df97b43f1758b7fb3e31a68707f67
Tuan Tuan LE <tlcong@linagora.com>
2020-07-31 10:56:11 +0700

    #58 remove useless dependency injection

--- src/modules/linagora.esn.user.status/frontend/app/user-status.run.js
+++ src/modules/linagora.esn.user.status/frontend/app/user-status.run.js
@@ -7,7 +7,7 @@ require('./services/user-status-websocket.service.js');
 
   angular.module('linagora.esn.user-status').run(run);
 
-  function run($interval, $rootScope, userStatusSyncService, userStatusWebsocketService, session, USER_STATUS_SYNC_INTERVAL) {
+  function run($interval, userStatusSyncService, userStatusWebsocketService, session, USER_STATUS_SYNC_INTERVAL) {
     session.ready.then(function() {
       userStatusWebsocketService.listen();
       $interval(userStatusSyncService.synchronize, USER_STATUS_SYNC_INTERVAL);
