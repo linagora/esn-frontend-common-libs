cdca3867bdb972f30727943e24b18ec0d6d59459
Tuan Tuan LE <tlcong@linagora.com>
2020-07-31 14:12:33 +0700

    #58 rename app.config.js to app.routes.js
    Since this file configure routes for linagora.esn.account module

--- src/modules/linagora.esn.account/frontend/js/app.js
+++ src/modules/linagora.esn.account/frontend/js/app.js
@@ -1,15 +1,14 @@
-(function(angular){
-  'use strict';
+'use strict';
 
-  angular.module('linagora.esn.account', [
-    'esn.router',
-    'op.dynamicDirective',
-    'esn.core',
-    'esn.ui',
-    'esn.http',
-    'esn.notification',
-    'linagora.esn.oauth.consumer']);
-})(angular);
+angular.module('linagora.esn.account', [
+  'esn.router',
+  'op.dynamicDirective',
+  'esn.core',
+  'esn.ui',
+  'esn.http',
+  'esn.notification',
+  'linagora.esn.oauth.consumer'
+]);
 
 require('../../../../frontend/js/modules/esn.router.js');
 require('../../../../frontend/components/dynamic-directive/src/dynamic-directive.js');
@@ -18,7 +17,7 @@ require('../../../../frontend/js/modules/ui.js');
 require('../../../../frontend/js/modules/http.js');
 require('../../../../frontend/js/modules/notification.js');
 require('../../../linagora.esn.oauth.consumer');
-require('./app.config.js');
+require('./app.routes.js');
 require('./app.run.js');
 require('./constants.js');
 require('./controllers.js');
