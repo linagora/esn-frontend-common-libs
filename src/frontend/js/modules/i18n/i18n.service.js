de2e7adf765fc089e93df18709fd5915af623d92
Tuan Tuan LE <tlcong@linagora.com>
2020-08-12 17:40:18 +0700

    #83 ES6ify the i18n.service.js file

--- src/frontend/js/modules/i18n/i18n.service.js
+++ src/frontend/js/modules/i18n/i18n.service.js
@@ -6,10 +6,10 @@ require('./i18n-string.service.js');
 angular.module('esn.i18n')
   .factory('esnI18nService', function($translate, EsnI18nString, esnConfig, ESN_I18N_DEFAULT_LOCALE, ESN_I18N_DEFAULT_FULL_LOCALE, ESN_I18N_FULL_LOCALE) {
     return {
-      getLocale: getLocale,
-      getFullLocale: getFullLocale,
-      translate: translate,
-      isI18nString: isI18nString
+      getLocale,
+      getFullLocale,
+      translate,
+      isI18nString
     };
 
     function getLocale() {
@@ -19,7 +19,7 @@ angular.module('esn.i18n')
     function getFullLocale(callback) {
       return esnConfig('core.language', ESN_I18N_DEFAULT_LOCALE)
         .then(function(locale) {
-          var fullLocale = ESN_I18N_FULL_LOCALE.hasOwnProperty(locale) ? ESN_I18N_FULL_LOCALE[locale] : ESN_I18N_DEFAULT_FULL_LOCALE;
+          const fullLocale = ESN_I18N_FULL_LOCALE.hasOwnProperty(locale) ? ESN_I18N_FULL_LOCALE[locale] : ESN_I18N_DEFAULT_FULL_LOCALE;
 
           return callback && typeof callback === 'function' ? callback(fullLocale) : fullLocale;
         });
