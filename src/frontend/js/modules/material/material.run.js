ae055356da72787b50d2f4f9ec842bbfef74cfd8
Tuan Tuan LE <tlcong@linagora.com>
2020-08-31 13:43:44 +0700

    https://github.com/OpenPaaS-Suite/esn-frontend-account/issues/54 caching template must be done in run block

--- src/frontend/js/modules/material/material.run.js
+++ src/frontend/js/modules/material/material.run.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('esn.material')
-  .config(cacheTemplate);
+  .run(cacheTemplate);
 
 function cacheTemplate($templateCache) {
   $templateCache.put('images/mdi/mdi.svg', require('../../../images/mdi/mdi.svg'));
