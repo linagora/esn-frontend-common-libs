2c26d3bcbe258a97255f9069c05401b060296078
Tuan Tuan LE <tlcong@linagora.com>
2020-09-01 16:16:02 +0700

    #109 remove unnecessary dependencies injection

--- src/frontend/js/modules/header/profile-menu/profile-menu.module.js
+++ src/frontend/js/modules/header/profile-menu/profile-menu.module.js
@@ -1,9 +1,6 @@
 'use strict';
 
 angular.module('esn.profile-menu', [
-  'material.components.button',
-  'material.components.menu',
-  'material.components.icon',
   'esn.session',
   'esn.avatar'
 ]);
