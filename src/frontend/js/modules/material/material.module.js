4fe4c5bc0db543b678495131f711d2e5f0427f71
Tuan Tuan LE <tlcong@linagora.com>
2020-09-01 16:16:56 +0700

    #109 inject missing angular material modules

--- src/frontend/js/modules/material/material.module.js
+++ src/frontend/js/modules/material/material.module.js
@@ -1,7 +1,10 @@
 'use strict';
 
 angular.module('esn.material', [
-  'material.core'
+  'material.core',
+  'material.components.button',
+  'material.components.menu',
+  'material.components.icon'
 ]);
 
 require('./material.config.js');
