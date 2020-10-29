05b231760bd1f718ad9015b4dc23aaca3071a9f6
Michael BAILLY <mbailly@linagora.com>
2020-10-29 15:51:00 +0100

    OpenPaaS-Suite/esn-frontend-calendar#241 Cache authentication token
    For direct connections between a frontend & the DAV server, there is a
    need to cache authentication token, otherwise any DAV request will be
    preprocessed with a call to get the authentication token.

--- src/frontend/js/modules/authentication.js
+++ src/frontend/js/modules/authentication.js
@@ -1,17 +1,46 @@
-(function(angular) {
-  'use strict';
+angular.module('esn.authentication', ['esn.http'])
+  .factory('tokenAPI', function(esnRestangular) {
 
-  angular.module('esn.authentication', ['esn.http'])
-    .factory('tokenAPI', function(esnRestangular) {
+    // https://github.com/linagora/openpaas-esn/blob/master/backend/core/auth/token.js#L3
+    // token TTL is 60 seconds server-side. We keep a lower value to get over
+    // network lags
+    const SERVER_TTL = 60 * 1000;
+    const NETWORK_LAG = 15 * 1000;
+    const CACHE_MAX_TTL = SERVER_TTL - NETWORK_LAG;
+    const cachedToken = { promise: null, timestamp: 0 };
 
-      function getNewToken() {
-        return esnRestangular.one('authenticationtoken').get();
-      }
+    return {
+      getNewToken
+    };
 
-      return {
-        getNewToken: getNewToken
-      };
-    });
-})(angular);
+    function getNewToken(resetCache = false) {
+      return resetCache ? getTokenNoCache() : getCachedToken();
+    }
+
+    function getCachedToken() {
+      const { promise, timestamp } = getCache();
+
+      return timestamp + CACHE_MAX_TTL > Date.now() ? promise : getTokenNoCache();
+    }
+
+    function getTokenNoCache() {
+      setCache(esnRestangular.one('authenticationtoken').get(), Date.now());
+
+      cachedToken.promise.catch(() => {
+        setCache(null);
+      });
+
+      return cachedToken.promise;
+    }
+
+    function setCache(promise, timestamp = 0) {
+      cachedToken.promise = promise;
+      cachedToken.timestamp = timestamp;
+    }
+
+    function getCache() {
+      return cachedToken;
+    }
+  });
 
 require('./http.js');
