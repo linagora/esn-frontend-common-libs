0b6ccf4bd73c20fdd01ac5a6e0a413857cf6d547
Fabien MOYON <fmoyon@linagora.com>
2020-12-08 11:37:27 +0100

    #61 remove popover in chips

--- src/frontend/js/modules/chips/chips.run.js
+++ src/frontend/js/modules/chips/chips.run.js
@@ -8,8 +8,6 @@
       [
         '<div',
         '    class="esn-chip"',
-        '    profile-popover-card="{{ data }}"',
-        '    profile-popover-card-show-mobile',
         '>',
         '  <span ng-bind="$getDisplayText()"></span>',
         '  <i class="mdi mdi-close-circle" ng-click="$removeTag()"></i>',
