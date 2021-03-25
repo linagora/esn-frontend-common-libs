e8be99539c2527ca3cddb1559078951a50dd39e2
Huy TA QUOC <htquoc@linagora.com>
2021-03-25 17:23:59 +0700

    Now the mdMenuBackdropFix directive has flexible z index

--- src/frontend/js/modules/esn.md-menu.backdrop.js
+++ src/frontend/js/modules/esn.md-menu.backdrop.js
@@ -5,15 +5,21 @@ angular.module('esn.md-menu.backdrop', [])
       link
     };
 
-    function link(scope, element) {
+    function link(scope, element, attrs) {
+      observe();
+
       element.click(changeBackdropZIndex);
 
       function changeBackdropZIndex() {
+        observe();
+      }
+
+      function observe() {
         const mutationObserver = new MutationObserver((mutations, observer) => {
           mutations.forEach(mutation => {
             if (!mutation.addedNodes.length || mutation.addedNodes[0].nodeName.toLowerCase() !== 'md-backdrop') return;
 
-            mutation.addedNodes[0].style = 'z-index: 1999;';
+            mutation.addedNodes[0].style = attrs.mdMenuBackdropFix ? `z-index: ${attrs.mdMenuBackdropFix};` : 'z-index: 1999;';
 
             observer.disconnect();
           });
