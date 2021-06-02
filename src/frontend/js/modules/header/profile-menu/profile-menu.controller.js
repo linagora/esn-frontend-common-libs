7f629a7ce105791f5e1aca80d5ea86e6c6786b5c
Khaled FERJANI <kferjani@linagora.com>
2021-06-02 15:00:49 +0100

    #204 use a dynamic account link location

--- src/frontend/js/modules/header/profile-menu/profile-menu.controller.js
+++ src/frontend/js/modules/header/profile-menu/profile-menu.controller.js
@@ -4,7 +4,7 @@
   angular.module('esn.profile-menu')
     .controller('ESNProfileMenuController', ESNProfileMenuController);
 
-  function ESNProfileMenuController($scope, session, esnAvatarUrlService) {
+  function ESNProfileMenuController($scope, session, esnAvatarUrlService, APPLICATION_GRID_ACCOUNT_SPA_NAME) {
     var self = this;
 
     self.$onInit = $onInit;
@@ -12,6 +12,7 @@
 
     function $onInit() {
       self.avatarURL = esnAvatarUrlService.generateUrlByUserId(session.user._id, true);
+      self.accountUrl = _accountUrl();
 
       $scope.$on('avatar:updated', onAvatarUpdated);
     }
@@ -24,6 +25,24 @@
       if (user && user._id === session.user._id) {
         self.avatarURL = esnAvatarUrlService.generateUrlByUserId(user._id, true);
       }
+    }
+
+    function _accountUrl() {
+
+      if (window.openpaas && window.openpaas.ACCOUNT_SPA_URL) {
+        return window.openpaas.ACCOUNT_SPA_URL;
+      }
+
+      const appGridItems = window.openpaas && window.openpaas.APP_GRID_ITEMS || process.env.APP_GRID_ITEMS;
+
+      if (appGridItems) {
+        const applications = JSON.parse(appGridItems);
+        const accountApp = applications.find(({ name }) => name === APPLICATION_GRID_ACCOUNT_SPA_NAME);
+
+        if (accountApp) return accountApp.url;
+      }
+
+      return '/account/';
     }
   }
 })();
